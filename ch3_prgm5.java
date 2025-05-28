d50263beabc33083eab13a360ff79305bfffc8b4
Karthik G <g.k.karthik0305@gmail.com>
2025-05-28 21:47:26 +0530

    Add files via upload

---
+++ ch3_prgm5.java
@@ -0,0 +1,7 @@
+class ch3_prgm5{
+	public static void main(String[] args){
+		String myString = "thi string contains double and   triple spaces";
+		System.out.println(myString.indexOf("  "));
+		System.out.println(myString.indexOf("   ")); 
+	}
+}
